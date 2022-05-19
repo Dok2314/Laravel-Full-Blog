72be8462dad529c05514c7e8ecb9703eced5d1cb
Daniil <koversundanil@gmail.com>
2022-05-19 22:50:33 +0300

    Added block if to many attempts

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CommentsRequest;
 use App\Models\Article;
 use App\Models\Comment;
+use Auth;
+use Illuminate\Support\Facades\RateLimiter;
 
 class CommentsController extends Controller
 {
     public function store(CommentsRequest $request)
     {
         $article_id = $request->input('article_id');
-        $article = Article::where('id', $article_id)->first();
+        $article    = Article::where('id', $article_id)->first();
+        $user       = Auth::user();
 
         $comment = new Comment([
             'title' => $request->input('title'),
@@ -20,7 +23,31 @@ class CommentsController extends Controller
             'article_id' => $article_id
         ]);
 
-        $comment->save();
+        if(!isset($user)){
+            $executed = RateLimiter::attempt(
+                'comment:',
+                 1,
+                function() use($comment){
+                    $comment->save();
+                },
+            600);
+
+            if (!$executed) {
+                return back()->with('error', 'Авторизуйтесь чтобы отправлять более 1 коментария в 10 минут!');
+            }
+        }else{
+            $executed = RateLimiter::attempt(
+                'comment:'.$user->id,
+                5,
+                function() use($comment){
+                    $comment->save();
+                },
+                60);
+
+            if (!$executed) {
+                return back()->with('error', 'Можно отправить только 5 коментариев за 1 минуту!');
+            }
+        }
 
         return redirect()->route('articles.preview', $article)->with('success', sprintf(
             'Коментарий успешно добавлен!',
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\RateLimiter;
 
 class LoginController extends Controller
 {
@@ -19,11 +20,21 @@ class LoginController extends Controller
         $user = User::where('email', $email)->first();
         $name = $user->name ?? '';
 
-        if(Auth::attempt($request->only(['email', 'password']), $request->input('remember'))) {
-            return redirect()->route('user.admin')->with('success', sprintf(
-                'С возвращением %s',
-                $name
-            ));
+        $executed = RateLimiter::attempt(
+            'login:'.$user->id,
+            5,
+            function() use($request, $name){
+                if(Auth::attempt($request->only(['email', 'password']), $request->input('remember'))) {
+                    return redirect()->route('user.admin')->with('success', sprintf(
+                        'С возвращением %s',
+                        $name
+                    ));
+                }
+            },
+        120);
+
+        if (! $executed) {
+            return back()->with('error', 'Слишком много попыток, подождите 2 минуты!');
         }
 
         return back()->withErrors([
